3ce7506e1caa87f9dbe77afd450a95d66393bff4
Joseph <48150537+Josephbakulikira@users.noreply.github.com>
2021-10-03 13:20:54 +0530

    add escape event

--- main.py
+++ main.py
@@ -50,6 +50,9 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
 
     if counter > 10 and iterations is not max:
         iteration()
